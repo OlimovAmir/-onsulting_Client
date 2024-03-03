608568fcb84eaddc88910c39db451ed7cc95cd38
olimov Amir <olimov.amir@mail.ru>
2024-03-03 13:22:20 +0300

    has been created file our adventage

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
 function Footer() {
+    const textStyle = {
+        color: 'primary', // белый цвет шрифта
+        fontWeight: 'bold', // жирный стиль шрифта
+        textShadow: '1px 1px 1px black', // контур шрифта черным
+    }
   return (
-    <div>Footer</div>
+    <div>
+        <div className="container d-flex justify-content-center align-items-center p-4">
+                <div className="row text-center">
+                    <div className="col">
+                        <h1 style={textStyle} >Широкий профиль услуг</h1>
+                    </div>
+                </div>
+            </div>
+    </div>
   )
 }
 
---
+++ src/components/our_adventage/OurAdventage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function OurAdventage() {
+    const textStyle = {
+        color: 'primary', // белый цвет шрифта
+        fontWeight: 'bold', // жирный стиль шрифта
+        textShadow: '1px 1px 1px black', // контур шрифта черным
+    }
+  return (
+    <div>OurAdventage</div>
+  )
+}
+
+export default OurAdventage
