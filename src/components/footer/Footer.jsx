9550cda080ee0c35a7a21b116bdfe1ee661e77ba
olimov Amir <olimov.amir@mail.ru>
2024-03-03 19:30:30 +0300

    fix bag

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -39,19 +39,19 @@ function Footer() {
                 <div className="container">
                     <div className="row">
                         <div className="col-md-3">
-                            <p>Адрес</p>
+                            <p className={styles.footerP1}>Адрес</p>
                             <p>Таджикистан, город Худжанд</p>
                         </div>
                         <div className="col-md-3">
-                            <p>Время работы</p>
+                            <p  className={styles.footerP1}>Время работы</p>
                             <p>Понедельник - Суббота <br/> С 09:00 до 20:00</p>
                         </div>
                         <div className="col-md-3">
-                            <p>Телефоны</p>
+                            <p className={styles.footerP1}>Телефоны</p>
                             <p>+992 92 775 75 77</p>
                         </div>
                         <div className="col-md-3">
-                            <p>Мы в соцсетях</p>
+                            <p className={styles.footerP1}>Мы в соцсетях</p>
                         </div>
                     </div>
                 </div>
