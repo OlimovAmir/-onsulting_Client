e55bc5fa436fbcb3a432c00e35bca59af8cbde35
olimov Amir <olimov.amir@mail.ru>
2024-03-03 14:06:59 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ButtonBanner from './components/button/ButtonBanner';
 import Header from './components/header/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Service from './components/service/Service';
+import OurAdventage from './components/our_adventage/OurAdventage';
 
 function App() {
 
@@ -18,7 +19,11 @@ function App() {
         <section>
           <Service />
         </section>
+        <OurAdventage/>
       </main>
+      <footer>
+
+      </footer>
     </div>
   );
 }
--- src/components/our_adventage/OurAdventage.jsx
+++ src/components/our_adventage/OurAdventage.jsx
@@ -1,4 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css'; // Импорт базовых стилей Swiper
+import 'swiper/css/navigation'; // Импорт стилей для навигации
+import 'swiper/css/pagination'; // Импорт стилей для пагинации
+import 'swiper/css/scrollbar'; // Импорт стилей для скроллбара
+import foto1 from '../../images/business-concept-with-graphic.jpg';
+import foto2 from '../../images/istockphoto.jpg';
+import foto3 from '../../images/istockphoto2.jpg';
 
 function OurAdventage() {
     const textStyle = {
@@ -6,17 +14,44 @@ function OurAdventage() {
         fontWeight: 'bold', // жирный стиль шрифта
         textShadow: '1px 1px 1px black', // контур шрифта черным
     }
-  return (
-    <div>
-        <div className="container d-flex justify-content-center align-items-center p-4">
+    return (
+        <div>
+            <div className="container d-flex justify-content-center align-items-center p-4">
                 <div className="row text-center">
                     <div className="col">
-                        <h1 style={textStyle} >Широкий профиль услуг</h1>
+                        <h1 style={textStyle} >Вы можете быть уверенны за сроки и качество работ</h1>
                     </div>
                 </div>
             </div>
-    </div>
-  )
+            <div className="container">
+                <div>
+                    <Swiper
+                        spaceBetween={50}
+                        slidesPerView={3}
+                        navigation
+                        pagination={{ clickable: true }}
+                        scrollbar={{ draggable: true }}
+                    >
+                        <SwiperSlide>
+                            <div>
+                                <img src={foto1} alt="Description1" style={{ width: '100%', height: 'auto' }} /> <p>Text</p>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div>
+                                <img src={foto2} alt="Description1" style={{ width: '100%', height: 'auto' }} /> <p>Text</p>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div>
+                                <img src={foto3} alt="Description1" style={{ width: '100%', height: 'auto' }} /> <p>Text</p>
+                            </div>
+                        </SwiperSlide>
+                    </Swiper>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default OurAdventage
