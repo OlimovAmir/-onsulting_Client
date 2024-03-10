7bd659b6ad5c66c0bd8bf9c79243ad0d766adb73
olimov Amir <olimov.amir@mail.ru>
2024-03-10 13:06:32 +0300

    fix

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,41 +3,49 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import logo from '../../images/DD_Logo_JPG.jpg';
+import { Image } from 'react-bootstrap';
 function Header() {
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-    <Container>
-      <Navbar.Brand href="#home">D & D CONSULTING </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="#features">Главгая</Nav.Link>
-          <Nav.Link href="#pricing">О нас</Nav.Link>
-          <NavDropdown title="Наши услуги" id="collapsible-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">ИНВЕСТИЦИОННЫЙ КОНСАЛТИНГ И ФИНАНСИРОВАНИЕ</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">ВНУТРЕННИЙ АУДИТ</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">КОМПЛАEНС МЕНЕДЖМЕНТ</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">СИСТЕМА УПРАВЛЕНИЯ РИСКАМИ</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">СОВРЕМЕННОЕ КОРПОРАТИВНОЕ УПРАВЛЕНИЕ</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">СТРАТЕГИЧЕСКОЕ ПЛАНИРОВАНИЕ</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">БЮДЖЕТИРОВАНИЕ</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">ОБУЧЕНИЯ И ТРЕНИНГИ</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">
-            Спектр IT услуг 
-            </NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Nav>
-          <Nav.Link href="#deets">Новости</Nav.Link>
-          <Nav.Link eventKey={2} href="#memes">
-            Контакты
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Container>
-  </Navbar>
+      <Container>
+        <Image
+          src={logo}
+          alt="D&D Consulting Logo"
+          style={{
+            width: '70px',
+            height: 'auto',
+            borderRadius: '20%'
+          }} />
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Главгая</Nav.Link>
+            <Nav.Link href="#pricing">О нас</Nav.Link>
+            <NavDropdown title="Наши услуги" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">ИНВЕСТИЦИОННЫЙ КОНСАЛТИНГ И ФИНАНСИРОВАНИЕ</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">ВНУТРЕННИЙ АУДИТ</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">КОМПЛАEНС МЕНЕДЖМЕНТ</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">СИСТЕМА УПРАВЛЕНИЯ РИСКАМИ</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">СОВРЕМЕННОЕ КОРПОРАТИВНОЕ УПРАВЛЕНИЕ</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">СТРАТЕГИЧЕСКОЕ ПЛАНИРОВАНИЕ</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">БЮДЖЕТИРОВАНИЕ</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">ОБУЧЕНИЯ И ТРЕНИНГИ</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Спектр IT услуг
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Новости</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Контакты
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
 
