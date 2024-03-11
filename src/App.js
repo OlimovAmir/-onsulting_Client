f2bb69f16b469ca48054c5971f5c90d44ebeed70
olimov Amir <olimov.amir@mail.ru>
2024-03-11 16:51:46 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -6,9 +6,25 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Service from './components/service/Service';
 import OurAdventage from './components/our_adventage/OurAdventage';
 import Footer from './components/footer/Footer';
+import React, { useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+
 
 function App() {
+  const controls = useAnimation();
+  const { ref, inView } = useInView();
 
+  
+
+  useEffect(() => {
+    if (inView) {
+      // Анимация, которую вы хотите запустить при появлении в области видимости
+      controls.start({ opacity: 1, y: 0 });
+    }
+  }, [controls, inView]);
+
+ 
   return (
     <div className="App">
       <header className="App-header">
@@ -17,13 +33,26 @@ function App() {
         <ButtonBanner />
       </header>
       <main>
-        <section>
+        <motion.section
+          ref={ref}
+          initial={{ opacity: 0, y: 100 }} // Начальное состояние до появления в области видимости
+          animate={controls}
+          transition={{ duration: 1 }} // Время анимации
+        >
           <Service />
-        </section>
-        <OurAdventage/>
+        </motion.section>
+        <motion.div
+          ref={ref}
+          initial={{ opacity: 0, y: 100 }} // Начальное состояние до появления в области видимости
+          animate={controls}
+          transition={{ duration: 5 }} // Время анимации
+        >
+          <OurAdventage />
+        </motion.div>
+
       </main>
       <footer>
-        <Footer/>
+        <Footer />
       </footer>
     </div>
   );
